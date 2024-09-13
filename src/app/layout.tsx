3880202180ff6270bcce3e230b83f3ad1e5ee673
zainriaz892 <zainbhaisss356@gmail.com>
2024-09-13 13:47:47 +0500

    my project about my self

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
+import Header from "@/components/Header";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -25,10 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="inter.className">
         {children}
+        <Header/>
       </body>
     </html>
   );
